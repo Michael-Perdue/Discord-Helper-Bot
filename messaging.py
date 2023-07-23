c4dfbea0c2dd3fcc9d811caa63c58880f88ee0a7
Michael Perdue <michael0perdue@gmail.com>
2023-07-23 22:17:47 +0100

    added 2 more functions 1 which just resends the users message and the other which countdowns from a given number i.e it will put 5 4 3 2 1 all with a 1 second delay inbetween

--- messaging.py
+++ messaging.py
@@ -1,12 +1,23 @@
 import discord
 from discord.ext import commands
-
+from time import sleep
 class Messaging(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-    @commands.command(name="spam")
-    async def spam(self,ctx):
-        await ctx.send("test")
+
+    @commands.command(name="test")
+    async def test(self,ctx):
+        await ctx.send("Test")
+
+    @commands.command(name="mirror")
+    async def mirror(self,ctx,arg):
+        await ctx.send(str(arg))
+
+    @commands.command(name="countdown")
+    async def countdown(self,ctx,arg):
+        for x in range(int(arg),0,-1):
+            await ctx.send(x)
+            sleep(1)
 
 async def setup(bot):
     await bot.add_cog(Messaging(bot))
