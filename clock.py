6e251a7f880fe5b52f20318b9252889d0a47b29e
Michael Perdue <michael0perdue@gmail.com>
2023-07-29 21:47:50 +0100

    transitioned timer and countdown to slash command and made the timer take in hours seconds and minutes

--- clock.py
+++ clock.py
@@ -1,44 +1,53 @@
 import asyncio
 import time
-
 import discord
+from typing import Optional
 from discord.ext import commands
 from main import log_message
+import re
 
 class Clock(commands.Cog):
     stopwatches = {}
 
-    def __init__(self,bot):
+    def __init__(self,bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.command(name="countdown")
-    async def countdown(self,ctx,arg):
+    @commands.hybrid_command(name="countdown", description="Counts down from the number given.")
+    async def countdown(self, ctx: commands.Context, start: int) -> None:
         """
         This function deals with a user sending a !countdown command, and a number the bot then
         will send back a message of the number after minusing one after every second to work like a countdown.
         Example command: '!countdown 3' and the bot will respond with a message saying '3' then '2' then '1' all
-        with one second between.
+        with one second between and then send a message saying the countdown is done.
         :param ctx: the context of the message
-        :param arg: an int of the number you want the bot to countdown from
+        :param start: an int of the number you want the bot to countdown from
         """
-        log_message(ctx,"Countdown command:\n   time: "+str(arg)+" seconds\n")
-        for x in range(int(arg),0,-1):
-            await ctx.send(x)
+        log_message(ctx,"Countdown command:\n   time: "+str(start)+" seconds\n")
+        await ctx.send(start)
+        for x in range(start-1,0,-1):
             await asyncio.sleep(1)
+            await ctx.channel.send(x)
+        await asyncio.sleep(1)
+        await ctx.channel.send(ctx.author.mention + " countdown done")
 
-    @commands.command(name="timer")
-    async def timer(self,ctx,arg):
+    @commands.hybrid_command(name="timer", description="Informs you when the time is up.")
+    async def timer(self,ctx: commands.Context,hours: Optional[int] = 0,minutes: Optional[int] = 0,seconds: Optional[int] = 0) -> None:
         """
-        This function deals with a user sending a !timer command, and a number the bot then
+        This function deals with a user sending a !timer command, and a time length, the bot then
         will sleep(non-blocking) until that number of seconds have gone by.
         It will then send a message mentioning the user saying the timer is up.
-        Example command: '!timer 10' and the bot will wait 10 seconds before messaging saying @user timer finished.
+        Example command: '/timer 10' and the bot will wait 10 seconds before messaging saying @user timer finished.
         :param ctx: the context of the message
-        :param arg: an int of the number of seconds you want the bot to wait before it alerts you
+        :param hours: an int of the number of hours you want the timer to go off in
+        :param minutes: an int of the number of minutes you want the timer to go off in
+        :param seconds: an int of the number of seconds you want the timer to go off in
         """
-        log_message(ctx,"Timer command:\n   time: "+str(arg)+" seconds\n")
-        await asyncio.sleep(int(arg))
-        await ctx.send(str(ctx.message.author.mention) + " " + str(arg) + "s timer finished")
+        total_time = (hours*60*60)+(minutes*60)+seconds
+        translated_time = time.strftime("%H hours %M minutes %S seconds",time.gmtime(total_time)).replace("00 hours ","").replace("00 minutes ","")
+        await ctx.send("Timer started",ephemeral=True)
+        log_message(ctx,"Timer command:\n   time: "+ translated_time +"\n")
+        await asyncio.sleep(total_time)
+        await ctx.channel.send(str(ctx.message.author.mention) + " " + translated_time + " timer finished")
 
     @commands.command(name="stopwatch")
     async def stopwatch(self,ctx : commands.Context,arg):
@@ -77,5 +86,5 @@ class Clock(commands.Cog):
             log_message(ctx, "Invalid use of stopwatch command:\n   argument given: " + str(arg) + "\n")
 
 
-async def setup(bot):
+async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Clock(bot))
--- fun.py
+++ fun.py
@@ -1,22 +1,22 @@
-import asyncio
 import random
 from discord.ext import commands
 from main import log_message
+
 
 class Fun(commands.Cog):
 
     def __init__(self,bot: commands.Bot) -> None:
         self.bot = bot
 
-    @commands.hybrid_command(name="coin")
+    @commands.hybrid_command(name="coin",description="Flips a coin.")
     async def coin_flip(self,ctx: commands.Context,times: int) -> None:
         results = []
         for x in range(0,int(times)):
             results.append("Heads" if random.randrange(2) else "Tails")
         await ctx.send("The coin/s landed on **" + " ".join(results) + "**")
-        log_message(ctx,"Coinflip command used:\n   times: "+str(times)+"\n")
+        log_message(ctx,"Coin flip command used:\n   times: "+str(times)+"\n")
 
-    @commands.hybrid_command(name="roll")
+    @commands.hybrid_command(name="roll",description="Rolls a set of dice.")
     async def roll(self,ctx: commands.Context,amount: str,size: str) -> None:
         result = []
         for x in range(0,int(amount)):
@@ -24,8 +24,6 @@ class Fun(commands.Cog):
             result.append(str(roll)+"/"+size)
         await ctx.send(" The dice landed on **" + " ".join(result) + "**")
         log_message(ctx,"roll command used:\n   amount of dice: " + amount + "\n   sides of dice: " + size + "\n")
-
-
 
 
 async def setup(bot: commands.Bot) -> None:
