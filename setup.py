effe6e66dfac2f4ec6983fc94e47e8df9771b605
Michael Perdue <michael0perdue@gmail.com>
2023-08-14 14:24:52 +0100

    removed redundant setup file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='discordbot',
-    version='1.0',
-    packages=['discordbot'],
-    include_package_data=True,
-    zip_safe=False
-)
